8449753a7b93d1e0b204ea3d1c43be3f9ba61636
eliezerwohl <eliezerwohl@gmail.com>
2016-04-29 14:35:40 -0400

    added rootscope to controller, added rootscope.type that will keep track of what type of input needs to be used

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -1,17 +1,23 @@
 var app = angular.module('app', []);
 
-app.controller('create', function($scope) {
+app.controller('create', function($scope, $rootScope) {
 
 $scope.singleInput = function(){
+	$rootScope.type = "single";
 	angular.element(document.getElementById("target"))
 	.append("<h2> put question here for single input</h2> <input> ")
 }
 
 $scope.textArea = function(){
+	$rootScope.type = "text";
 angular.element(document.getElementById('target'))
 	.append("<h2> question for text area </h2> <input>  question </input>") 
-
 console.log("layla")
+}
+
+$scope.save = function(){
+
+	console.log($rootScope.type)
 }
 
 
