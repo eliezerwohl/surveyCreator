7be692a87050dcde776c26cb92891a9d83e85595
eliezerwohl <eliezerwohl@gmail.com>
2016-05-02 14:55:54 -0400

    added answer Schema

---
+++ server/models/answer.js
@@ -0,0 +1,15 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var AnswerSchema = new Schema({
+	_question: {
+		type:Schema.Types.ObjectId,
+		ref: "question"
+	},
+	//the person who answered
+	email:String,
+	//in array in case of mutiple answers
+	answer:[{type:String}]
+});
+
+var Answer = mongoose.model("Answer", AnswerSchema);
