06bc1dd3892333201febdd916881bf4716ab8621
eliezerwohl <eliezerwohl@gmail.com>
2016-05-03 21:49:25 -0400

    can now create user.  still need to encrypt password

--- server/controllers/homeController.js
+++ server/controllers/homeController.js
@@ -1,6 +1,16 @@
-var Info = require("../models/user");
+var User = require("../models/user");
 
 exports.signUp = function (req, res){
-	debugger
-console.log(req.body)
+	var newUser = new User({"firstName":req.body.firstName,
+													"lastName":req.body.lastName,
+													"email":req.body.email,
+													"password":req.body.password});
+	newUser.save(function (err, doc) {
+		if (err){
+			console.log(err)
+		}
+		else{
+			res.send(doc)
+		}
+	});
 }
