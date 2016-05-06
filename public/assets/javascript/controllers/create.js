12adbac99451405bb03051c871d38ee0a008930a
eliezerwohl <eliezerwohl@gmail.com>
2016-05-06 15:48:52 -0400

    buttons on create survey page now associated with a input type, will be able to hide and show divs based on scope inputtype

--- public/assets/javascript/controllers/create.js
+++ public/assets/javascript/controllers/create.js
@@ -1,18 +1,24 @@
-app.controller('create', function() {
-	
-var idLength = 15
-var id = [];
-var bank = [ "1","2","3","4","5","6","7","8","9",'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-idCreator()
+app.controller('create', function($scope) {
 
-function idCreator(){
-	for (var i = 0; i < idLength; i++) {
-		var char = Math.floor(Math.random() * bank.length)
-		id.push(bank[char])
+	$scope.inputType=function(input){
+		$scope.selectedInput = input
+		console.log(input)
 	}
-}
-	if (id.length = idLength ){
-		var newId = id.join('')
-		console.log("new id is " + newId)
-	}
+	
+
+// var idLength = 15
+// var id = [];
+// var bank = [ "1","2","3","4","5","6","7","8","9",'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+// idCreator()
+
+// function idCreator(){
+// 	for (var i = 0; i < idLength; i++) {
+// 		var char = Math.floor(Math.random() * bank.length)
+// 		id.push(bank[char])
+// 	}
+// }
+// 	if (id.length = idLength ){
+// 		var newId = id.join('')
+// 		console.log("new id is " + newId)
+// 	}
 }) //end of controller
