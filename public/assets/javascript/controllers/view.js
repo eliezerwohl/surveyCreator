20ae7199c26796ce2d8f7d6c82354e9b9ab346d8
eliezerwohl <eliezerwohl@gmail.com>
2016-05-08 14:11:09 -0400

    getting info back, able to sort which object is supposed to be what

--- public/assets/javascript/controllers/view.js
+++ public/assets/javascript/controllers/view.js
@@ -5,6 +5,25 @@ app.controller("view", function($scope, $http, $state){
 			method:"GET",
 			url:"/previewSurvey"
 		}).then(function successCallback(response){
+			var data = response.data
+			
+			for (var i = 0; i < data.length; i++) {
+				debugger
+
+				if(data[i].type === "input"){
+					console.log("this is input")
+				}
+				else if(data[i].type === "textarea"){
+					console.log("textarea")
+				}
+				else if(data[i].type === "radio"){
+					console.log("radio")
+				}
+				else if(data[i].type === "checkbox"){
+					console.log("checkbox")
+				}
+				
+			}
 
 		}, function errorCallback(reponse){
 
