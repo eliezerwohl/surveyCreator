93c03ada72134a6090fd6d2728ba71530a562cdd
eliezerwohl <eliezerwohl@gmail.com>
2016-05-12 15:24:13 -0400

    realized couldn't do planned way of pushing in the backend the value of the checkboxes into the same document, storing them as an array BEFORe being sent

--- public/assets/javascript/controllers/view.js
+++ public/assets/javascript/controllers/view.js
@@ -1,6 +1,6 @@
 app.controller("view", function($scope, $http, $state){
 
-	function storeData(value, id, push){
+	function storeData(value, id){
 	$http({
 			method:"POST",
 			url:"/storeData",
@@ -15,7 +15,11 @@ app.controller("view", function($scope, $http, $state){
 	$scope.test = function(){
 
 		var inputs = document.getElementsByTagName('input');
-		debugger
+				function callback(number){
+					inputs.splice(number, 1)
+					inputs[number]
+				console.log(inputs)
+			}
 		var textarea = document.getElementsByTagName('textarea');
 		//need seperate loop, text area doesn't count as an input
 			for (var i = 0; i < textarea.length; i++) {
@@ -23,18 +27,38 @@ app.controller("view", function($scope, $http, $state){
 						var id = inputs[i].dataset.id
 						var push = false
 						console.log("this is a textarea" +value + id + push)
-						storeData(value, id, push)
+						storeData(value, id)
 			}
 			var l = inputs.length
 			
+
 			for (i = 0; i < l; i++) {
-				
-					if ((inputs[i].type === "checkbox") && (inputs[i].checked === true )){
-						var value = inputs[i].value
-						var id = inputs[i].dataset.id
-						var push = true
-						console.log("this is a checkbox" +value + id + push)
-						storeData(value, id, push)
+					
+					
+					if ((inputs[i].type === "checkbox")  && (inputs[i].dataset.number=== "0")){
+						var valueArray = [];
+						var k = 0;
+						for (var j = 0; j < l; j++) {
+							k++
+							
+						
+								if ((inputs[j].type === "checkbox") && (inputs[i].dataset.id === inputs[j].dataset.id) && (inputs[j].checked === true )){
+										valueArray.push(inputs[j].value)
+										debugger
+									
+						
+										
+								}
+
+							}
+						if ( k === inputs.length){
+							storeData(value, id)
+						}
+					
+						// var id = inputs[i].dataset.id
+						// var push = true
+						// console.log("this is a checkbox" +value + id + push)
+						// storeData(value, id)
 						//add another value so it will know to push it on
 
 					}
@@ -42,16 +66,14 @@ app.controller("view", function($scope, $http, $state){
 						// console.log("the value ofo this radion is" + inputs[i].value)
 						var value = inputs[i].value
 						var id = inputs[i].dataset.id
-						var push = false
 						console.log("this is a radio" +value + id + push)
-						storeData(value, id, push)
+						storeData(value, id)
 
 					}
 					//if it's just an input
 						else if (inputs[i].dataset.type === "input") {
 						var value = inputs[i].value
 						var id = inputs[i].dataset.id
-						var push = false
 						console.log("this is single" +value + id + push)
 					}
 			}
@@ -95,7 +117,7 @@ app.controller("view", function($scope, $http, $state){
 					var length = data[i].options.length 
 					var preview = "<h2>" + data[i].text + "</h2>";
 					for (var j = 0;  j < length; j++){
-						preview += "<input data-id='" +  data[i]._id 
+						preview += "<input data-number='" + j +"' data-id='" +  data[i]._id 
 						+"' value ='" + data[i].options[j] + "' type = 'checkbox'>" + data[i].options[j]
 						//minus 1 because j start at zero
 						if (j === length - 1){
