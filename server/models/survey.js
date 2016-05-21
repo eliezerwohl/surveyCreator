0f6e2fae4018874f2ff0b00ea81a2295049bfb99
eliezerwohl <eliezerwohl@gmail.com>
2016-05-20 23:24:51 -0400

    applied css to page, need to even it out.  still unsure about background color

--- server/models/survey.js
+++ server/models/survey.js
@@ -12,6 +12,8 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var SurveySchema= new Schema({
+	name:String,
+	date:{type:Date, Default:Date.now}
 	_user: {
 		type:Schema.Types.ObjectId,
 		ref: "User"
