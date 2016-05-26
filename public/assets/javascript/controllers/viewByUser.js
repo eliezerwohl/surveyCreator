1024e10914628de51e6ede5cd8d70e3cf77fc89b
eliezerwohl <eliezerwohl@gmail.com>
2016-05-26 18:33:24 -0400

    render by users function now works, appends to the target

--- public/assets/javascript/controllers/viewByUser.js
+++ public/assets/javascript/controllers/viewByUser.js
@@ -1,69 +1,59 @@
 app.controller("viewByUser", function($scope, $rootScope, $http, $state){
 
 	function renderByUser(question, answer){
-		debugger
+		var section = "<h2>" + question + "</h2>"
+		+ "<h4>" + answer + "</h4>"
+			angular.element(document.getElementById('target'))
+		.append(section) 
 	}
-	$scope.viewByUserResults=function(){
-
-	$http({
-		method: "GET",
-		url: "/viewThisUser"
-	}).then(function successCallback(docs) {
-		var thisUserId = docs.data 
-			$http({
-		method: "GET",
-		url: "/viewAllQuestions"
-	}).then(function successCallback(response) {
-
-		for (var i = 0; i < response.data.length; i++) {
-			
-			for (var j = 0; j < response.data[i]._answer.length; j++) {
-				
-				console.log($rootScope.thisUserId)
-				
-				if (response.data[i]._answer[j].randomId === thisUserId){
-					
-					var question = response.data[i].text 
-					var answer = response.data[i]._answer[j].answer[0]
-					renderByUser(question, answer)
-				}
-			}
-		}
-
-
-	})
-
-	})
 	
 
-}
+	$scope.viewByUserResults = function() {
+		$http({
+			method: "GET",
+			url: "/viewThisUser"
+		}).then(function successCallback(docs) {
+			var thisUserId = docs.data
+			$http({
+				method: "GET",
+				url: "/viewAllQuestions"
+			}).then(function successCallback(response) {
+
+				for (var i = 0; i < response.data.length; i++) {
+
+					for (var j = 0; j < response.data[i]._answer.length; j++) {
+
+
+						if (response.data[i]._answer[j].randomId === thisUserId) {
+
+							var question = response.data[i].text
+							var answer = response.data[i]._answer[j].answer[0]
+							renderByUser(question, answer)
+						}
+					}
+				}
+			})
+
+		})
+	}
 	$rootScope.thisUser= function(id){
-		
 			$http({
 			method:"POST",
 			url:"/thisUser",
 			data:{"id":id}
 		}).then(function successCallback(response){
-			
-			$state.go("viewByUser")
-
-			
+			$state.go("viewByUser")			
 		}, function errorCallback(response){
-
 			
 		});
-
 	}
 $scope.userList= function(){
-
 	$http({
 			method:"GET",
 			url:"/userList",
 		}).then(function successCallback(response){
 			
 			$scope.data = response.data 
-
-			
 		}, function errorCallback(response){
 			
 		});
